331a493be3d417b635b5fb502403b1a0720a98f2
Wasiu-hub <contactbow@yahoo.com>
2023-09-24 01:53:41 +0100

    our first test- it fails

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
+const artistRouter = require("./routes/artist"); // Import the artistRouter
 
 const app = express();
-
+// Middleware to parse JSON requests
 app.use(express.json());
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
+
+// Use the artist router for routes starting with '/artists'
+app.use("/artists", artistRouter); // directs all artists to artistController
 
 module.exports = app;
---
+++ src/routes/artist.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const artistRouter = express.Router();
+const artistController = require("../controllers/artist");
+
+// Define a POST route that connects to the createArtist controller function
+artistRouter.post("/", artistController.createArtist);
+
+module.exports = artistRouter;
